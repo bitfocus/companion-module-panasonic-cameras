9631c8f33404d6bbf99a4d54d9e6fa129547e7cb
Phillip Ivan Pietruschka <ivanpietruschka@gmail.com>
2024-09-26 08:50:29 +1000

    new upgrade script update gitignore

--- src/upgrades.js
+++ src/upgrades.js
@@ -1,1 +1,27 @@
-export const upgradeScripts = []
+export const upgradeScripts = [
+    function addSetIncDecVariables(_context, props) {
+		const result = {
+			updatedActions: [],
+			updatedConfig: null,
+			updatedFeedbacks: [],
+		}
+
+		for (const action of props.actions) {
+			switch (action.actionId) {
+				case 'iris':
+                case 'ped':
+                case 'pedRed':
+                case 'pedBlue':
+                case 'gainRed':
+                case 'gainBlue':
+                case 'colorTemperature':
+					action.options.useVar = action.options.useVar === undefined ? false : action.options.useVar
+                    action.options.setVar = action.options.setVar === undefined ? '0' : action.options.setVar
+                    action.options.stepVar = action.options.stepVar === undefined ? '1' : action.options.stepVar                    
+					result.updatedActions.push(action)
+					break
+			}
+		}
+		return result
+	},
+]
